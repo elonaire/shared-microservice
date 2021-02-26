1b9acf5901ee02c17fc2350d20a1de8656a9c4e3
Elon Aseneka <elonsantos63@gmail.com>
2021-02-26 23:14:25 +0300

    feat(core): install node/nest mailer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
-import { FileUploadModule } from './file-upload/file-upload.module';
-import { fileUploadProviders } from './file-upload/file-upload.providers';
+import {MailerModule} from '@nestjs-modules/mailer';
+import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {DatabaseModule} from './database/database.module';
+import {FileUploadModule} from './file-upload/file-upload.module';
+import {fileUploadProviders} from './file-upload/file-upload.providers';
 
 @Module({
-  imports: [FileUploadModule, DatabaseModule, ConfigModule.forRoot()],
+  imports: [
+    FileUploadModule,
+    DatabaseModule,
+    ConfigModule.forRoot(),
+    MailerModule.forRoot({
+      transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, ...fileUploadProviders],
 })
